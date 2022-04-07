f37830b9e2d366aa8dbd2258a97374776fe4a53f
Brittany Ganser <15brganser@gmail.com>
2022-04-06 21:47:19 -0600

    created package.json

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # read-me-generator
+
+// run npm install to get dependencies
---
+++ app.js
@@ -0,0 +1,4 @@
+//imports file system from node
+const fs = require("fs");
+//imports inquirer from npm
+const inquirer = require("inquirer");
